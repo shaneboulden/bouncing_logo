65a503295876b22b3ac16d45bf72703911ea32f1
shaneboulden <shane.boulden@gmail.com>
2023-10-16 08:28:30 +1000

    Update logo

--- sketch.js
+++ sketch.js
@@ -12,29 +12,29 @@ let velocity = {
 let dvd;
 
 function preload() {
-  dvd = loadImage("assets/white_dvd.png");
+  logo = loadImage("assets/logo.png");
 }
 
 function setup() {
   // this creates the initial window and loads the image
   createCanvas(window.innerWidth, window.innerHeight);
+
+  logo.resize(194, 125);
 }
 
 function draw() {
   background(0);
-  image(dvd, coord.x, coord.y);
+  image(logo, coord.x, coord.y);
 
   // changes x velocity and color when an edge is impacted
-  if(coord.x + dvd.width > width || coord.x < 0) {
+  if(coord.x + logo.width > width || coord.x < 0) {
     velocity.x = (-velocity.x);
-    tint(random(0, 255), random(0, 255), random(0, 255));
   }
   coord.x = coord.x + velocity.x;
 
   // changes y velocity and color when an edge is impacted
-  if (coord.y + dvd.height > height || coord.y < 0) {
+  if (coord.y + logo.height > height || coord.y < 0) {
     velocity.y = (-velocity.y);
-    tint(random(0, 255), random(0, 255), random(0, 255));
   }
   coord.y = coord.y + velocity.y
 }
